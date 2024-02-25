0cb55491a1cf072ac557c8725e0080e249535650
iheb-haddad <hadediheb9@gmail.com>
2024-02-25 04:45:59 +0100

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
 
   return (
     <div className={`app ${finished && 'sucessBox'}`}
-    style={{backgroundImage : 'background-image: linear-gradient(rgba(199, 150, 150, 0.7),rgba(199, 150, 150, 0.7)),url("./background.jpg")'}}>
+    style={{backgroundImage : 'linear-gradient(rgba(199, 150, 150, 0.7),rgba(199, 150, 150, 0.7)),url("./background.jpg")'}}>
       {!finished ? 
         <div className="book">
           {/* If page 2 is opened, display the ingredients page */}
